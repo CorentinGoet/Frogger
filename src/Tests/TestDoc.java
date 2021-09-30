44b6b196ea55cd6a756efd89e345bccf9f0fbd1a
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-09-30 16:20:17 +0200

    added config files for IntelliJ

---
+++ src/Tests/TestDoc.java
@@ -0,0 +1,20 @@
+package Tests;
+
+public class TestDoc {
+    /**
+     * Ceci est un test de la documentation Javadoc
+     * @author CorentinGoet
+     * @version 0.0001
+     */
+
+    /**
+     * Affiche "Hello World!"
+     */
+    public static void HelloWorld(){
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld();
+    }
+}
