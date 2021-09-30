feb7aab33c3f3bda7755340db9f08f03d68d2c8f
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2021-09-30 19:27:49 +0200

    added class Frog

--- src/frog/Frog.java
+++ src/frog/Frog.java
@@ -1,4 +1,36 @@
 package frog;
 
+
+/**
+ * Classe modelisant la grenouille.
+ */
 public class Frog {
+    private int lane;
+    private int current_case;
+
+    /**
+     * Constructeur de la classe Frog
+     * @param lane voie sur laquelle la grenouille se trouve
+     * @param current_case case de la voie ou se trouve la grenouille
+     */
+    public Frog(int lane, int current_case) {
+        this.lane = lane;
+        this.current_case = current_case;
+    }
+
+    /**
+     * Retourne les coordonnées de la grenouille (case, lane)
+     * @return coords
+     */
+    public int[] getCoords(){
+        int[] coords = new int[2];
+        coords[0] = this.current_case;
+        coords[1] = this.lane;
+        return coords;
+    }
+
+    @Override
+    public String toString() {
+        return ",'O',";
+    }
 }
--- src/util/Case.java
+++ src/util/Case.java
@@ -1,5 +1,7 @@
 package util;
 
+import frog.Frog;
+
 /**
  * Cette classe correspond aux cases.
  *
@@ -8,6 +10,7 @@ package util;
  */
 public class Case {
     private int size;
+    private Frog content;
 
     /**
      * Constructeur de la classe Case précisant une taille.
@@ -35,8 +38,29 @@ public class Case {
         return size;
     }
 
+    /**
+     * Retourne le contenu de la case.
+     * @return content
+     */
+    public Frog getContent() {
+        return content;
+    }
+
+    /**
+     * Met a jour le contenu de la case.
+     * @param content
+     */
+    public void setContent(Frog content) {
+        this.content = content;
+    }
+
     @Override
     public String toString() {
-        return "|  |";
+        if(this.content == null){
+            return "      ";
+        }else{
+            return "|" + this.getContent() + "|";
+        }
+
     }
 }
