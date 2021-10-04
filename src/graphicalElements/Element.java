50430dffb92c13228d76736b766fb1f797cc6b2b
Emile <emile.berteloot@ensta-bretagne.org>
2021-10-04 12:05:29 +0100

    J'ai essayé de partir du projet fait en TD mais même en important toutes les librairies nécessaires, rien ne se lance quand je run.... Aled aidez-moi

--- src/graphicalElements/Element.java
+++ src/graphicalElements/Element.java
@@ -1,4 +1,35 @@
 package graphicalElements;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Element {
+public class Element extends Application{
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        // create a labels
+        Label label1 = new Label("Name");
+        Label label2 = new Label("ID");
+        Label label3 = new Label("Address");
+        Label label4 = new Label("Phone Number");
+        // create a Grid pane
+        GridPane r = new GridPane();
+        //Add the labels to the stack panes
+        r.add(label1,0,0);
+        r.add(label2,0,2);
+        r.add(label3,0,4);
+        r.add(label4,0,6);
+        // create a scene
+        Scene sc = new Scene(r, 200, 200);
+        // set the scene
+        primaryStage.setScene(sc);
+        primaryStage.show();
+    }
 }
